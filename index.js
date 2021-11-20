98f833d41447e0d54cf64b2e10d84821d34c2078
Sergii <iakovyshyn.s@gmail.com>
2021-11-20 15:58:17 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+const tabs = document.querySelectorAll(".tabs");
+const tab = document.querySelectorAll(".tab");
+const panel = document.querySelectorAll(".panel");
+
+function onTabClick(event) {
+
+    for (let i = 0; i < tab.length; i++) {
+        tab[i].classList.remove("active");
+    }
+    for (let i = 0; i < panel.length; i++) {
+        panel[i].classList.remove("active");
+    }
+    event.target.classList.add('active');
+    let classString = event.target.getAttribute('data-target');
+    console.log(classString);
+    document.getElementById('panels').getElementsByClassName(classString)[0].classList.add("active");
+}
+
+for (let i = 0; i < tab.length; i++) {
+    tab[i].addEventListener('click', onTabClick, false);
+}
